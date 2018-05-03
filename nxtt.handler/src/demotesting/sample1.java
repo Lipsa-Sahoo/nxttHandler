316217dd4b1b48cf26adbff2b54c260bd36b201f
lipsa-sahoo <sahoolipsa676@gmail.com>
2018-05-03 13:11:29 +0530

    Added add method to DemoTesting class

---
+++ nxtt.handler/src/demotesting/sample1.java
@@ -0,0 +1,16 @@
+package demotesting;
+
+public class sample1 {
+	static int a = 40, b = 20;
+
+	public static void add(){
+			int add = a + b;
+			System.out.println("add = "+add);
+		}
+
+
+	public static void main(String[] args) {
+		add();
+	}
+	
+}
